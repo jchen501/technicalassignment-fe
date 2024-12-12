c6f9875e8144f5f280b95758ef3a777208f0d57e
jchen501 <jiayinggg98@gmail.com>
2024-12-12 11:28:28 +0800

    Upload a CSV file with upload progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState<File>();
+  const [progress, setProgress] = useState<any>({ started: false, prog: 0});
+  const [message, setMessage] = useState<String>();
+
+  function handleFile(event){
+    setFile(event.target.files[0])
+  }
+
+  function handleSubmit(event){
+    if(!file){
+      setMessage('No file selected')
+      return;
+    }
+    event.preventDefault()
+    const url = 'http://localhost:8080/upload';
+    const formData = new FormData();
+    formData.append('file', file);
+
+    setMessage('Uploading...')
+    setProgress(prevState => {
+      return {...prevState, started: true}
+    })
+
+    axios.post(url, formData, {
+      onUploadProgress: (e) => {
+        setProgress(prevState => {
+          return {...prevState, prog: e.progress*100}
+        })
+      },
+      headers: {
+        'Custom-Header': 'value',
+      }
+    })
+    .then(res => {
+      setMessage("Upload success")
+      console.log(res.data)
+    })
+    .catch(err => {
+      setMessage("Upload failed")
+      console.log(err)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <h1>Upload File</h1>
+          <input type="file" onChange={handleFile}/>
+          <button type="submit">Upload</button>
+
+          <div>
+          { progress.started && <progress max="100" value={progress.prog}></progress>}
+          { message }
+          </div>
+        </form>
     </div>
   );
 }
