6cc65eb9325b8b0067791aba30053d14e826af67
jchen501 <jiayinggg98@gmail.com>
2024-12-13 13:43:21 +0800

    List data with pagination and search bar.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('axios');
+
+test.only('Page loaded', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText('Upload File');
+  expect(title).toBeInTheDocument();
+
+  //Upload button disabled
+  const submitButton = await screen.findByTestId('submit-button');
+  expect(submitButton).toBeDisabled();
+
+  //List Data button disabled
+  const listButton = await screen.findByTestId('list-button');
+  expect(listButton).toBeDisabled();
+})
+
+test.only('Verify upload file', async () => {
+  const file = new File(['data'], "data.csv", { type: "text/csv" });
+  const { getByTestId } = render(<App />);
+
+  let input = getByTestId("file-input");
+  await waitFor(() =>
+    fireEvent.change(input, {
+      target: { files: [file] },
+    })
+  );
+  userEvent.upload(input, file);
+  expect(input.files.length).toBe(1);
+
+  //Upload button enabled when file uploaded
+  const submitButton = await screen.findByTestId('submit-button');
+  expect(submitButton).toBeEnabled();
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
+import React, { useState } from 'react';
+import './App.css';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 function App() {
   const [file, setFile] = useState<File>();
   const [progress, setProgress] = useState<any>({ started: false, prog: 0});
   const [message, setMessage] = useState<String>();
+  const [data, setData] = useState<String[]>([]);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [filePresent, setFilePresent] = useState(false);
+  const url = 'http://localhost:8080/';
 
   function handleFile(event){
     setFile(event.target.files[0])
   }
 
   function handleSubmit(event){
-    if(!file){
-      setMessage('No file selected')
-      return;
-    }
     event.preventDefault()
-    const url = 'http://localhost:8080/upload';
     const formData = new FormData();
     formData.append('file', file);
 
@@ -27,10 +26,10 @@ function App() {
       return {...prevState, started: true}
     })
 
-    axios.post(url, formData, {
-      onUploadProgress: (e) => {
+    axios.post(url + 'upload', formData, {
+      onUploadProgress: (nativeEvent) => {
         setProgress(prevState => {
-          return {...prevState, prog: e.progress*100}
+          return {...prevState, prog: nativeEvent.loaded!*100}
         })
       },
       headers: {
@@ -39,6 +38,7 @@ function App() {
     })
     .then(res => {
       setMessage("Upload success")
+      setUploadSuccess(true)
       console.log(res.data)
     })
     .catch(err => {
@@ -47,18 +47,66 @@ function App() {
     })
   }
 
+  function handleList(){
+    axios.get(url + 'list', { params: { filename: file?.name} })
+      .then(function (response) {
+        setData(response.data)
+        setFilePresent(true)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.2},
+    { field: 'name', headerName: 'Name', flex: 0.5},
+    { field: 'email', headerName: 'Email', flex: 0.5},
+    { field: 'body', headerName: 'Body', flex: 1},
+  ]
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
           <h1>Upload File</h1>
-          <input type="file" onChange={handleFile}/>
-          <button type="submit">Upload</button>
-
+          <input type="file" onChange={handleFile} data-testid='file-input'/>
+          <button type="submit" disabled={!file} data-testid="submit-button">Upload</button>
           <div>
           { progress.started && <progress max="100" value={progress.prog}></progress>}
           { message }
           </div>
         </form>
+        
+        <div>
+            <button type="submit" onClick={handleList} disabled={!uploadSuccess} data-testid="list-button">List Data</button>
+        </div>
+        {
+          filePresent ? 
+          <div>
+            <DataGrid 
+              columns={columns}
+              rows={data}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 10,
+                  },
+                },
+              }}
+              pageSizeOptions={[10, 20, 30, 50]}
+              getRowHeight={() => 'auto'}
+              disableColumnFilter
+              disableColumnSelector
+              disableDensitySelector
+              slots={{ toolbar: GridToolbar }}
+              slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                },
+              }}
+            />
+          </div> : null
+        }
     </div>
   );
 }
